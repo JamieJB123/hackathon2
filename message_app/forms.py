38c2320ed1772b941689b8d0d9cb9db4576d3478
dav-noble <davidnoble26@gmail.com>
2025-08-15 11:35:13 +0100

    Add: crispyforms. create view, form, and template for messages/admin page

--- message_app/forms.py
+++ message_app/forms.py
@@ -2,6 +2,15 @@ from django import forms
 from .models import Message
 
 class MessageForm(forms.ModelForm):
+    scheduled_at = forms.DateTimeField(
+        widget=forms.DateTimeInput(
+            attrs={
+                'type': 'datetime-local',  # HTML5 calendar and time picker
+                'class': 'form-control'
+            }
+        )
+    )
+    
     class Meta:
         model = Message
         fields = ['content', 'featured_image', 'scheduled_at', 'message_type']
