9aa9b2788d36000517d36dfa7a593cf9d4a6f1fb
dav-noble <davidnoble26@gmail.com>
2025-08-15 15:20:06 +0100

    Add: delete functionality to admin page, renamed address for admin page to messages-admin

--- message_app/urls.py
+++ message_app/urls.py
@@ -3,10 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.HomePage.as_view(), name='home'),
-
+    path('delete/<int:pk>/', views.message_delete, name='message_delete'),
     path('display/', views.display, name='display'),
     path('api/current-message/', views.get_message_api, name='get_message_api'),
-
-    path('admin/', views.AdminPage, name='admin_page'),
-
+    path('messages-admin/', views.AdminPage, name='admin_page'),
 ]
